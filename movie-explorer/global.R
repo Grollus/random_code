ef2828e7974bb5a4538282aff7193c87906b3f66
Grollus <athoul01@gmail.com>
2015-08-19 21:16:33 -0400

    dplyr usage example with shiny. Also the sort of visualization I might want to use for Louisville mapping

---
+++ movie-explorer/global.R
@@ -0,0 +1,10 @@
+# Variables that can be put on the x and y axis
+
+axis_vars <- c(
+  "Tomato Meter" = "Meter",
+  "Numeric Rating" = "Rating",
+  "Number of reviews" = "Reviews",
+  "Dollars at box office" = "BoxOffice",
+  "Year" = "Year",
+  "Length (minutes)" = "Runtime"
+)
